6dd195f748813aabeea13c2f963ae083ad601dc0
Rudra <rudramadhabnayak0@gmail.com>
2024-06-21 14:15:43 +0530

    comment

---
+++ javascrift/class1/index.js
@@ -0,0 +1,101 @@
+let name="john doe";
+let age=30;
+console.log(name,age)
+
+let x=BigInt(567);
+console.log(x);
+console.log(typeof(x));
+
+const ab=null;
+console.log(ab)
+console.log(typeof(ab));
+
+let myjob;
+console.log(myjob);
+console.log(typeof(myjob))
+
+ let s="string";
+ console.log(s);
+ console.log(typeof(s));
+
+ let y=Symbol("hello")
+ console.log(y);
+ console.log(y.description);
+ console.log(typeof(y));
+ 
+ Rudra={
+    college:"gita",
+    rollno:2102139,
+    age:20,
+    
+ }
+ console.log(Rudra);
+ console.log(Rudra["college"]);
+ console.log(Rudra["rollno"]);
+ console.log(Rudra["age"]);
+//operator
+ let a=5;
+ let b=6;
+ console.log("a+b=",a+b);
+ console.log("++a",a++);
+ console.log(a)
+ console.log("++a",++a);
+ let cond1=a > b;
+ let cond2=a==6;
+ console.log("condition",cond1 && cond2);
+ console.log("!(6<5)=",!(a<b));
+
+//  conditional statement
+
+let Age=25;
+ if(age>18){
+    console.log("can vote")
+ }
+//  let mode="darck"
+//  let color;
+//  if(mode === "darck"){
+//     color="black"
+//  }
+//  if(mode==="llight"){
+//     color="white"
+//  }
+//  console.log(color);
+
+//  let mode="darck"
+//  let color;
+//  if(mode === "darck"){
+//     color="black"
+//  }
+//  if(mode==="llight"){
+//     color="white"
+//  }
+//  console.log(color);
+
+//ifelse
+//odd or even
+// let num=10;
+// let n=num%2
+// if(n===0){
+//     console.log(num,"is even");
+// }
+// else{
+//     console.log(num,"is odd");
+// }
+
+//elseif
+let mode="dark";
+let color;
+if(mode==="dark"){
+    color="black";
+}else if(mode=="blue"){
+    color="blue";
+
+}
+else if(mode=="pink"){
+    color='pink';
+
+}
+else{
+    color="white";
+}
+console.log(color);
---
+++ javascrift/note.md
@@ -0,0 +1,13 @@
+What is javascript and why weused it?
+
+-javascript is logic based programing language where we can creat our code functionnality in that. -javascript is a high level programing language for build web pages. -Now we used the ES6(echma script 6)version of javascript,in other javascript. -runtime enviroment of javascript is node js.
+
+Variable:-
+
+-variable is container to store some data. -In javascript we have 3 types of variables 1-let 2-var 3-const
+
+let :-
+
+-let is a type of variable which is used for changing the variable name later. -Now these days, most of the cases we used let for creating variable. -let is a block scope code so we have been using let for mostof cases.
+
+var:- -Var is a type of variable which is also used for chaging the variable later stage. -Var is used in oldest browser so now a days we are dont use var most of the time. const:- -const is a type of variable where we can't change our date further. -const means const to store some data like number,integer etc...
